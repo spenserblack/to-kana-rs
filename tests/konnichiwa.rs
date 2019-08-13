8b901408c82a524995cc37735ec3ea2e89c0b374
Spenser Black <spenserblack01@gmail.com>
2019-08-13 14:12:04 -0400

    Add integration tests

---
+++ tests/konnichiwa.rs
@@ -0,0 +1,30 @@
+use to_kana::{
+    hira,
+    kata,
+};
+
+#[test]
+fn hiragana_konnichiwa_per_syllable() {
+    let ko = hira("ko").unwrap();
+    let n = hira("n").unwrap();
+    let ni = hira("ni").unwrap();
+    let chi = hira("ti").unwrap();
+    let wa = hira("ha").unwrap();
+
+    let konnichiwa = format!("{}{}{}{}{}", ko, n, ni, chi, wa);
+
+    assert_eq!("こんにちは", konnichiwa);
+}
+
+#[test]
+fn katakana_konnichiwa_per_syllable() {
+    let ko = kata("ko").unwrap();
+    let n = kata("n").unwrap();
+    let ni = kata("ni").unwrap();
+    let chi = kata("ti").unwrap();
+    let wa = kata("ha").unwrap();
+
+    let konnichiwa = format!("{}{}{}{}{}", ko, n, ni, chi, wa);
+
+    assert_eq!("コンニチハ", konnichiwa);
+}
