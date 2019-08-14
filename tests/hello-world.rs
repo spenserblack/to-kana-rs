03c78b29fdcfa4c9bbc6b026e1c53fc2f784b43b
Spenser Black <spenserblack01@gmail.com>
2019-08-14 11:10:04 -0400

    Add Hello World test

---
+++ tests/hello-world.rs
@@ -0,0 +1,12 @@
+use to_kana::{
+    hira,
+    kata,
+};
+
+#[test]
+fn hello_world_mixed() {
+    let hello = hira("konnichiha").unwrap();
+    let world = kata("wa-rudo").unwrap();
+
+    assert_eq!("こんにちはワールド", format!("{}{}", hello, world));
+}
