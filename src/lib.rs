6e3af118bd0da1c12eba84dac10f67ff4898313c
Spenser Black <spenserblack01@gmail.com>
2019-08-15 15:05:30 -0400

    Add 、
    Related to #14

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,7 @@ fn syllable_divider<'a>(s: &'a str) -> Vec<&'a str> {
     let mut start_index = 0;
     for (end_index, c) in s.char_indices() {
         match c {
-            '-' | 'a' | 'e' | 'i' | 'o' | 'u' => {
+            '-' | 'a' | 'e' | 'i' | 'o' | 'u' | ',' => {
                 vec.push(&s[start_index..=end_index]);
                 start_index = end_index + 1;
             },
@@ -80,6 +80,7 @@ pub fn hira<'a>(s: &'a str) -> Result {
 
     for s in syllable_divider(s) {
         let kana = match s {
+            "," => String::from("、"),
             "-" => String::from("ー"),
             "a" => String::from("あ"),
             "i" => String::from("い"),
@@ -242,6 +243,7 @@ pub fn kata(s: &str) -> Result {
 
     for s in syllable_divider(s) {
         let kana = match s {
+            "," => String::from("、"),
             "-" => String::from("ー"),
             "a" => String::from("ア"),
             "i" => String::from("イ"),
@@ -638,5 +640,11 @@ mod tests {
     #[test]
     fn turn_katakana() {
         assert_eq!(Ok(String::from("ターン")), kata("ta-n"));
+    }
+
+    #[test]
+    fn comma() {
+        assert_eq!(Ok(String::from("、")), hira(","));
+        assert_eq!(Ok(String::from("、")), kata(","));
     }
 }
