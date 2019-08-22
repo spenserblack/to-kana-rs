9cdf21a07855cc8954e6ac2466bd9cd5df78904a
Spenser Black <spenserblack01@gmail.com>
2019-08-22 14:29:46 -0400

    Add little katakana auieo & yayuyo

--- src/small_kana.rs
+++ src/small_kana.rs
@@ -9,7 +9,8 @@ impl SmallKana for &str {
     fn small(self) -> Result {
         let utf16: Vec<u16> = self.encode_utf16().map(|u16char| {
             match u16char {
-                0x3042 ... 0x304A | 0x3084 ... 0x3088 if u16char % 2 == 0 => {
+                0x3042 ... 0x304A | 0x3084 ... 0x3088 |
+                0x30A2 ... 0x30AA | 0x30E4 ... 0x30E8 if u16char % 2 == 0 => {
                     u16char - 0x0001
                 },
                 _ => u16char,
