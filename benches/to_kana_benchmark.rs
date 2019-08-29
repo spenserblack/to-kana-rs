c9c2f96e9c8ddade58917bc712d02b9269fac60b
Spenser Black <spenserblack01@gmail.com>
2019-08-29 14:26:51 -0400

    Add aiueo and mirror benches for kata

--- benches/to_kana_benchmark.rs
+++ benches/to_kana_benchmark.rs
@@ -6,12 +6,27 @@ use criterion::black_box;
 
 use to_kana::*;
 
-fn a_hira_benchmark(c: &mut Criterion) {
+fn hira_benchmark(c: &mut Criterion) {
     c.bench_function(
         "a hira",
         |b| b.iter(|| hira(black_box("a"))),
     );
+    c.bench_function(
+        "aiueo hira",
+        |b| b.iter(|| hira(black_box("aiueo"))),
+    );
 }
 
-criterion_group!(benches, a_hira_benchmark);
+fn kata_benchmark(c: &mut Criterion) {
+    c.bench_function(
+        "a kata",
+        |b| b.iter(|| kata(black_box("a"))),
+    );
+    c.bench_function(
+        "aiueo kata",
+        |b| b.iter(|| kata(black_box("aiueo"))),
+    );
+}
+
+criterion_group!(benches, hira_benchmark, kata_benchmark);
 criterion_main!(benches);
