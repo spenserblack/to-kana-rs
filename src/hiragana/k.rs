4da2c0c363785ed1537eb88c1f78791e6f743153
Spenser Black <spenserblack01@gmail.com>
2019-09-11 16:09:36 -0400

    Add きゃきゅきょ

--- src/hiragana/k.rs
+++ src/hiragana/k.rs
@@ -6,6 +6,8 @@ use std::str::Chars;
 
 use crate::Error;
 
+use super::small::small_y;
+
 pub fn k(hiragana: &mut String, characters: &mut Enumerate<Chars>) -> Result<(), Error> {
     match characters.next() {
         Some((_, 'a')) => hiragana.push('か'),
@@ -13,6 +15,10 @@ pub fn k(hiragana: &mut String, characters: &mut Enumerate<Chars>) -> Result<(),
         Some((_, 'u')) => hiragana.push('く'),
         Some((_, 'e')) => hiragana.push('け'),
         Some((_, 'o')) => hiragana.push('こ'),
+        Some((_, 'y')) => {
+            hiragana.push('き');
+            small_y(hiragana, characters)?;
+        }
         Some((i, c)) => return Err(format!("character at {} not allowed: {}", i, c)),
         None => return Err(String::from("'k' must be followed by a vowel")),
     }
--- src/hiragana/small.rs
+++ src/hiragana/small.rs
@@ -13,6 +13,18 @@ pub fn small(hiragana: &mut String, characters: &mut Enumerate<Chars>) -> Result
         Some((_, 'u')) => hiragana.push('ぅ'),
         Some((_, 'e')) => hiragana.push('ぇ'),
         Some((_, 'o')) => hiragana.push('ぉ'),
+        Some((_, 'y')) => small_y(hiragana, characters)?,
+        Some((i, c)) => return Err(format!("character at {} not allowed: {}", i, c)),
+        None => return Err(String::from("Unexpected end of string")),
+    }
+    Ok(())
+}
+
+pub fn small_y(hiragana: &mut String, characters: &mut Enumerate<Chars>) -> Result<(), Error> {
+    match characters.next() {
+        Some((_, 'a')) => hiragana.push('ゃ'),
+        Some((_, 'u')) => hiragana.push('ゅ'),
+        Some((_, 'o')) => hiragana.push('ょ'),
         Some((i, c)) => return Err(format!("character at {} not allowed: {}", i, c)),
         None => return Err(String::from("Unexpected end of string")),
     }
