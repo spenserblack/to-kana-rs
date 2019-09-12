35e2077561a76b1d2d406b9e6d8d14145b145fb8
Spenser Black <spenserblack01@gmail.com>
2019-09-12 11:56:04 -0400

    Add zy* pattern

--- src/hiragana/z.rs
+++ src/hiragana/z.rs
@@ -19,6 +19,17 @@ pub fn z(hiragana: &mut String, characters: &mut Enumerate<Chars>) -> Result<(),
         Some((_, 'u')) => hiragana.push('ず'),
         Some((_, 'e')) => hiragana.push('ぜ'),
         Some((_, 'o')) => hiragana.push('ぞ'),
+        Some((_, 'y')) => {
+            hiragana.push('じ');
+            match characters.next() {
+                Some((_, 'a')) => hiragana.push(small::YA),
+                Some((_, 'u')) => hiragana.push(small::YU),
+                Some((_, 'o')) => hiragana.push(small::YO),
+                Some((_, 'e')) => hiragana.push(small::E),
+                Some((i, c)) => return Err(unexpected_char_error(i, c)),
+                None => return Err(unexpected_end_of_string()),
+            }
+        }
         Some((i, c)) => return Err(unexpected_char_error(i, c)),
         None => return Err(unexpected_end_of_string()),
     }
