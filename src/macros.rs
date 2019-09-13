e996c0e2a3f2d5932d4d10b3a8568bff0c9347f3
Spenser Black <spenserblack01@gmail.com>
2019-09-13 16:19:55 -0400

    Add failing example

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,12 @@
+/// Functions similarly to `format!`, except it converts args to Hiragana and
+/// Katakana.
+///
+/// # Example
+///
+/// ```
+/// let s = to_kana!("konnichiha in hiragana: {:h}, konbanha in katakana: {:k}", "konnichiha", "konbanha");
+/// assert_eq!("konnichiha in hiragana: こんにちは, konbanha in katakana: コンバンハ", s);
+/// ```
 #[macro_export]
 macro_rules! to_kana {
     ($format_str:tt, $($args:tt)*) => (panic!("Macro not working yet!"));
