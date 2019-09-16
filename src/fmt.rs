8f68fdb5979e9daca2c05d7d6142acc9e92c73cc
Spenser Black <spenserblack01@gmail.com>
2019-09-16 10:28:02 -0400

    Add link to example

--- src/fmt.rs
+++ src/fmt.rs
@@ -8,7 +8,7 @@ use super::{
 };
 
 pub fn format_kana(format_str: &str, args: Vec<&str>) -> Result {
-    let format_finder = Regex::new(r"\{:.*\}").unwrap(); // TODO Use lazy_static to improve performance
+    let format_finder = Regex::new(r"\{:.*\}").unwrap(); // TODO Use lazy_static to improve performance: https://docs.rs/regex/1.3.1/regex/#example-avoid-compiling-the-same-regex-in-a-loop
     let mut index: usize = 0;
     let mut out = String::from(format_str);
     println!("Format finder: {:?}", format_finder.find(&out));
