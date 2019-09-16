f6a280ac50d6850890214af03c3b2df8d17de14d
Spenser Black <spenserblack01@gmail.com>
2019-09-16 09:45:59 -0400

    Set up expected format behaviors

--- src/fmt.rs
+++ src/fmt.rs
@@ -21,7 +21,29 @@ mod tests {
 
     #[test]
     fn format_kana_test() {
-        let s = format_kana("konnichiha in hiragana: {:h}, konbanha in katakana: {:k}", vec!["konnichiha", "konbanha"]).unwrap();
+        let s = format_kana("hiragana: {:H}, katakana: {:K}", vec!["konnichiha", "konbanha"]).unwrap();
         assert_eq!("konnichiha in hiragana: こんにちは, konbanha in katakana: コンバンハ", s);
+        let s = format_kana("hiragana: {:h}, katakana: {:k}", vec!["yo", "yo"]).unwrap();
+        assert_eq!("konnichiha in hiragana: ょ, konbanha in katakana: ョ", s);
+        let s = format_kana("katakana: {:K/2}", vec!["konnichiha", "konbanha"]).unwrap();
+        assert_eq!("konbanha in half-width katakana: ｺﾝﾊﾞﾝﾊ", s);
+    }
+
+    #[test]
+    fn format_small_kana_test() {
+        let s = format_kana("hiragana: {:h}, katakana: {:k}", vec!["yo", "yo"]).unwrap();
+        assert_eq!("konnichiha in hiragana: ょ, konbanha in katakana: ョ", s);
+    }
+
+    #[test]
+    fn format_half_width_test() {
+        let s = format_kana("katakana: {:K/2}", vec!["konbanha"]).unwrap();
+        assert_eq!("konbanha in half-width katakana: ｺﾝﾊﾞﾝﾊ", s);
+    }
+
+    #[test]
+    fn format_half_width_small_test() {
+        let s = format_kana("katakana: {:k/2}", vec!["yo"]).unwrap();
+        assert_eq!("konbanha in half-width katakana: ｮ", s);
     }
 }
