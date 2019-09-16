fff7a62e3aed4780d3ccdcd3575440c993627552
Spenser Black <spenserblack01@gmail.com>
2019-09-16 13:18:25 -0400

    Add supported formats documentation

--- src/macros.rs
+++ src/macros.rs
@@ -11,6 +11,14 @@
 ///     to_kana!("{:H} and {:K}", "hiragana", "katakana").unwrap()
 /// );
 /// ```
+///
+/// # Supported formats
+/// - `:H` => Hiragana (ひらがな)
+/// - `:K` => Katakana (カタカナ)
+/// - `:K/2` => Half-width Katakana (ｶﾀｶﾅ)
+/// - `:h` => Small Hiragana (ぁぃぅぇぉゃゅょっ)
+/// - `:k` => Small Katakana (ァィゥェォャュョッ)
+/// - `:k/2` => Small, Half-width Katakana (ｧｨｩｪｫｬｭｮｯ)
 #[macro_export]
 macro_rules! to_kana {
     ($format_str:tt, $($args:expr),*) => ($crate::fmt::format_kana($format_str, vec![$($args),*]))
