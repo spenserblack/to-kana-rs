e806abbc8fd64f1f22f276417494480490c3b561
Spenser Black <spenserblack01@gmail.com>
2019-09-16 09:34:44 -0400

    Change return type to result

--- src/fmt.rs
+++ src/fmt.rs
@@ -1,4 +1,17 @@
-pub fn format_kana(format_str: &str, args: Vec<&str>) -> String {
+use regex::Regex;
+
+use super::{
+    ToKana,
+    HalfWidth,
+    SmallKana,
+    Result,
+};
+
+pub fn format_kana(format_str: &str, args: Vec<&str>) -> Result {
+    let format_finder = Regex::new(r"\{:[A-z]\}").unwrap(); // TODO Use lazy_static to improve performance
+    let mut index: usize = 0;
+    let mut out = String::from(format_str);
+    println!("Format finder: {:?}", format_finder.find(&out));
     panic!("Args not implemented: {:?}", args);
 }
 
@@ -8,7 +21,7 @@ mod tests {
 
     #[test]
     fn format_kana_test() {
-        let s = format_kana("konnichiha in hiragana: {:h}, konbanha in katakana: {:k}", vec!["konnichiha", "konbanha"]);
+        let s = format_kana("konnichiha in hiragana: {:h}, konbanha in katakana: {:k}", vec!["konnichiha", "konbanha"]).unwrap();
         assert_eq!("konnichiha in hiragana: こんにちは, konbanha in katakana: コンバンハ", s);
     }
 }
