e48f50424200000db4270cfcdf41a829fa8f60dc
Spenser Black <spenserblack01@gmail.com>
2019-09-16 13:38:49 -0400

    Add tests to be used with README

--- tests/readme.rs
+++ tests/readme.rs
@@ -13,3 +13,16 @@ fn last_is_number_1() {
         format!("{}{}{}{}", rust, ga, number_1, i_think),
     );
 }
+
+#[test]
+fn the_new_way_main() {
+    let hello_world = to_kana!("{:H}{:K}", "konnichiha,", "wa-rudo!").unwrap();
+    assert_eq!("こんにちは、ワールド！", hello_world);
+}
+
+#[test]
+fn the_new_way_half_width() {
+    let katakana = to_kana!("Half-width: {:K/2} works too!", "katakana")
+        .unwrap();
+    assert_eq!("Half-width: ｶﾀｶﾅ works too!", katakana);
+}
